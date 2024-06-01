450be77aab7819f6d2a780f4a42e6bec94b15990
root <root@Mhmd.virtualbox.org>
2024-06-01 15:21:17 +0300

    Adding more files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
     const dct = {};
+    
     $('input:checkbox').change(function() {
-	if ($(this).is(':checked')) {
-	    dct[$(this).attr('data-id')] = $(this).attr('data-name');
-	    console.log($(this).attr('data-name'));
-	}
-	else {
-	    delete dct[$(this).attr('data-id')];
-	}
-	$('.amenities h4').text(Object.values(dct).join(', '));
+        if ($(this).is(':checked')) {
+            dct[$(this).attr('data-id')] = $(this).attr('data-name');
+            console.log($(this).attr('data-name'));
+        } else {
+            delete dct[$(this).attr('data-id')];
+        }
+        $('.amenities h4').text(Object.values(dct).join(', '));
     });
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(response) {
-	if response.status == "OK" &&
-	    $('div#api_status').hasClass('available'); {
-		$('div#api_status').addClass('available');
-	}
-	else {
-	    $('div#api_status').removeClass('available');
-	}
-    });	
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data, status) {
+        if (status === "success") {
+            if (data.status === "OK") {
+                $('div#api_status').addClass('available');
+		$('div#api_status').css('background-color', '#ff545f');
+            } else {
+                $('div#api_status').removeClass('available');
+            }
+            console.log(data.status);
+        } else {
+            $('div#api_status').removeClass('available');
+            console.log('Failed to get API status');
+        }
+    }); 
 });
